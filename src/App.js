c238b3bed2091e5d309f0a2c35f92626088ae53b
ASUS <guptasanskar2001@gmail.com>
2022-02-15 18:34:06 +0530

    post

--- src/App.js
+++ src/App.js
@@ -1,20 +1,24 @@
+import React from 'react';
+
 import './App.css';
 import Navbar from './Components/Navbar'
 import Introduction from './Components/Introduction';
 import WithoutLosses from './Components/WithoutLosses';
+import Button from './Components/Button';
 
 
 
 function App() {
-    
+
 
   return (
+    
     <>
       <Navbar />
       <Introduction />
       <WithoutLosses />
     </>
-    
+
   );
 }
 
--- src/Components/GeneratingStationData.jsx
+++ src/Components/GeneratingStationData.jsx
@@ -1,4 +1,7 @@
+import { borderRadius, grid, typography } from '@mui/system';
+import Typography from "@mui/material/Typography"
 import React, { useState } from 'react'
+import TextField from "@mui/material/TextField"
 
 export default function GeneratingStationData(props) {
     const [a, seta] = useState();
@@ -6,41 +9,53 @@ export default function GeneratingStationData(props) {
     const [c, setc] = useState();
     const [pMin, setPMin] = useState();
     const [pMax, setPMax] = useState();
-
+  
     function changeA(event) {
-        seta(event.target.value);
+       
+      console.log(event.target.value);  
+      seta(event.target.value);
+        console.log(a)
+        props.updateData('a',parseInt(event.target.value),props.num-1);
     }
     function changeB(event) {
         setb(event.target.value);
+        props.updateData('b',parseInt(event.target.value),props.num-1);
     }
     function changeC(event) {
         setc(event.target.value);
+        props.updateData('c',parseInt(event.target.value),props.num-1);
     }
     function changePMin(event) {
         setPMin(event.target.value);
+        props.updateData('pmin',parseInt(event.target.value),props.num-1);
     }
     function changePMax(event) {
         setPMax(event.target.value);
+        props.updateData('pmax',parseInt(event.target.value),props.num-1);
     }
+
+     
 
   return (
     <>
+
+    
     <div>
       <h3>Generating Station {props.num}</h3>
       <label>Cost function (in Rs.): </label>
-      <input className = "mx-2 inp" onChange={changeA} value = {a}></input>
+      <input type="number" className = "mx-2 inp" onChange={changeA} value = {a}></input>
       <label>P<sup>2</sup> + </label>
-      <input className = "mx-2 inp" onChange={changeB} value = {b}></input>
+      <input type="number" className = "mx-2 inp" onChange={changeB} value = {b}></input>
       <label>P + </label>
-      <input className = "mx-2 inp" onChange={changeC} value = {c}></input>
+      <input type="number" className = "mx-2 inp" onChange={changeC} value = {c}></input>
     </div>
     <div>
       <label>P_min (in MW) : </label>
-      <input className = "mx-2 inp" onChange={changePMin} value = {pMin}></input>
+      <input type="number" className = "mx-2 inp" onChange={changePMin} value = {pMin}></input>
     </div>
     <div>
       <label>P_max (in MW) : </label>
-      <input className = "mx-2 inp" onChange={changePMax} value = {pMax}></input>
+      <input type="number" className = "mx-2 inp" onChange={changePMax} value = {pMax}></input>
     </div>
     </>
   )
